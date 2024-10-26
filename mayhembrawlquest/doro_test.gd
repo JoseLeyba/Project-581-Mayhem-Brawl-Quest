2b0fa9d543c8beaf75f3a3bacba99d2a864f0402
JoseLeyba <123515981+JoseLeyba@users.noreply.github.com>
2024-10-26 14:59:19 -0500

    Update doro_test.gd

--- mayhembrawlquest/doro_test.gd
+++ mayhembrawlquest/doro_test.gd
@@ -1,27 +1,22 @@
-extends KinematicBody2D
-extends KinematicBody2D
-
+extends CharacterBody2D
 const SPEED = 500.0
 const JUMP_VELOCITY = -400.0
-
+# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-onready var sword = $Sword # Assume Sword is a child node
-
 func _physics_process(delta):
-    velocity.x = 0
-    if Input.is_key_pressed(KEY_D):
-        velocity.x = SPEED
-    elif Input.is_key_pressed(KEY_A):
-        velocity.x = -SPEED
-
-    if not is_on_floor():
-        velocity.y += gravity * delta
-
-    if Input.is_key_pressed(KEY_W) and is_on_floor():
-        velocity.y = JUMP_VELOCITY
-
-    move_and_slide(velocity, Vector2.UP)
-
-    # Trigger attack on button press
-    if Input.is_action_just_pressed("attack_p1"):
-        sword.attack()
+	
+	# if no key is pressed for left or right, x velocity is zero
+	velocity.x = 0
+	if Input.is_key_pressed(KEY_D):
+		velocity.x = SPEED
+	elif Input.is_key_pressed(KEY_A):
+		velocity.x = -SPEED
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	# Handle jump.w
+	if Input.is_key_pressed(KEY_W) and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	move_and_slide()
