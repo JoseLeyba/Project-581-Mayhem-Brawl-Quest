3f33533740a5b7f60d4d4dcece970d5821648b4b
JoseLeyba <123515981+JoseLeyba@users.noreply.github.com>
2024-10-26 14:58:53 -0500

    Update Player2_test.gd

--- mayhembrawlquest/Player2_test.gd
+++ mayhembrawlquest/Player2_test.gd
@@ -1,35 +1,23 @@
-extends KinematicBody2D  # Make sure this is only declared once
-
+extends CharacterBody2D
 const SPEED = 500.0
 const JUMP_VELOCITY = -400.0
-
+# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-onready var sword = $Sword  # Sword node should be a child node
-
-func _ready():
-    # Initialize the player's weapon here if needed
-    sword = preload("res://Sword.tscn").instance()
-    add_child(sword)
-
 func _physics_process(delta):
-    velocity.x = 0
-    if Input.is_key_pressed(KEY_L):
-        velocity.x = SPEED
-    elif Input.is_key_pressed(KEY_J):
-        velocity.x = -SPEED
+	
+	# if no key is pressed for left or right, x velocity is zero
+	velocity.x = 0
+	if Input.is_key_pressed(KEY_L):
+		velocity.x = SPEED
+	elif Input.is_key_pressed(KEY_J):
+		velocity.x = -SPEED
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	# Handle jump.w
+	if Input.is_key_pressed(KEY_I) and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	move_and_slide()
 
-    if not is_on_floor():
-        velocity.y += gravity * delta
-
-    if Input.is_key_pressed(KEY_I) and is_on_floor():
-        velocity.y = JUMP_VELOCITY
-
-    move_and_slide(velocity, Vector2.UP)
-    
-    # Trigger attack on button press
-    if Input.is_action_just_pressed("attack_p2"):
-        sword.attack()
-
-func attack():
-    if sword:
-        sword.attack()  # Call the sword's attack function
