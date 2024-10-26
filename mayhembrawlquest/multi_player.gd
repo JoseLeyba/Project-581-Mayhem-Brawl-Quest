d476b47931ddc295edc24a4233abd80da4b5b3fd
JoseLeyba <123515981+JoseLeyba@users.noreply.github.com>
2024-10-26 15:11:23 -0500

    Add files via upload

--- mayhembrawlquest/multi_player.gd
+++ mayhembrawlquest/multi_player.gd
@@ -11,5 +11,8 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_button_pressed() -> void:
+func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://startMenu.tscn") # Replace with function body.
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://testWorld2.tscn") # Replace with function body.
