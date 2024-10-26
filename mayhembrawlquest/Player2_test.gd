d0ff3b818dfb4bf965f97032be7e49e9d716cdb9
JoseLeyba <123515981+JoseLeyba@users.noreply.github.com>
2024-10-26 14:25:08 -0500

    Update Player2_test.gd

--- mayhembrawlquest/Player2_test.gd
+++ mayhembrawlquest/Player2_test.gd
@@ -1,11 +1,16 @@
-extends KinematicBody2D
+extends KinematicBody2D  # Make sure this is only declared once
 
 const SPEED = 500.0
 const JUMP_VELOCITY = -400.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var velocity = Vector2()
-onready var sword = $Sword # Assume Sword is a child node
+onready var sword = $Sword  # Sword node should be a child node
+
+func _ready():
+    # Initialize the player's weapon here if needed
+    sword = preload("res://Sword.tscn").instance()
+    add_child(sword)
 
 func _physics_process(delta):
     velocity.x = 0
@@ -21,7 +26,11 @@ func _physics_process(delta):
         velocity.y = JUMP_VELOCITY
 
     move_and_slide(velocity, Vector2.UP)
-
+    
     # Trigger attack on button press
     if Input.is_action_just_pressed("attack_p2"):
         sword.attack()
+
+func attack():
+    if sword:
+        sword.attack()  # Call the sword's attack function
